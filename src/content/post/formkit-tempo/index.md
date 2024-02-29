8c1374338a3fca75593402be672d46c8beaa71c3
melodyclue <melody.router@gmail.com>
2024-02-29 17:18:12 +0900

    add @formkit/tempo

---
+++ src/content/post/formkit-tempo/index.md
@@ -0,0 +1,15 @@
+---
+title: "Tempoはdate-fnsのalternaitveに十分なりうる"
+publishDate: "29 Feburary 2024"
+description: ""
+tags: ["tempo"]
+draft: false
+---
+
+Tempoという日付ライブラリ
+
+https://github.com/formkit/tempo
+
+- まず何よりドキュメントが見やすい(date-fnsに比べて)
+- タイムゾーンの扱いがシンプル(date-fnsに比べて)
+- 軽い
--- src/pages/og-image/[slug].png.ts
+++ src/pages/og-image/[slug].png.ts
@@ -5,8 +5,8 @@ import { Resvg } from "@resvg/resvg-js";
 import { siteConfig } from "@/site-config";
 import { getAllPosts, getFormattedDate } from "@/utils";
 
-import RobotoMono from "@/assets/roboto-mono-regular.ttf";
-import RobotoMonoBold from "@/assets/roboto-mono-700.ttf";
+import NotoSansJpSemiBold from "@/assets/NotoSansJP-SemiBold.ttf";
+import { format } from "@formkit/tempo";
 
 const ogOptions: SatoriOptions = {
 	width: 1200,
@@ -14,44 +14,26 @@ const ogOptions: SatoriOptions = {
 	// debug: true,
 	fonts: [
 		{
-			name: "Roboto Mono",
-			data: Buffer.from(RobotoMono),
-			weight: 400,
-			style: "normal",
-		},
-		{
-			name: "Roboto Mono",
-			data: Buffer.from(RobotoMonoBold),
-			weight: 700,
+			name: "notosansjp",
+			data: Buffer.from(NotoSansJpSemiBold),
+			weight: 600,
 			style: "normal",
 		},
 	],
 };
 
 const markup = (title: string, pubDate: string) =>
-	html`<div tw="flex flex-col w-full h-full bg-[#1d1f21] text-[#c9cacc]">
-		<div tw="flex flex-col flex-1 w-full p-10 justify-center">
-			<p tw="text-2xl mb-6">${pubDate}</p>
-			<h1 tw="text-6xl font-bold leading-snug text-white">${title}</h1>
+	html`<div class="flex h-full w-full flex-col bg-[#1d1f21] text-[#c9cacc]"
+	style={{
+		fontFamily: "notosansjp",
+	}}>
+		<div class="flex w-full max-w-[90%] flex-1 flex-col justify-center p-14">
+			<p class="mb-6 text-2xl">${pubDate}</p>
+			<h1 class="text-4xl leading-snug text-[#efefef]">${title}</h1>
 		</div>
-		<div tw="flex items-center justify-between w-full p-10 border-t border-[#2bbc89] text-xl">
-			<div tw="flex items-center">
-				<svg height="60" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 272 480">
-					<path
-						d="M181.334 93.333v-40L226.667 80v40l-45.333-26.667ZM136.001 53.333 90.667 26.667v426.666L136.001 480V53.333Z"
-						fill="#B04304"
-					></path>
-					<path
-						d="m136.001 119.944 45.333-26.667 45.333 26.667-45.333 26.667-45.333-26.667ZM90.667 26.667 136.001 0l45.333 26.667-45.333 26.666-45.334-26.666ZM181.334 53.277l45.333-26.666L272 53.277l-45.333 26.667-45.333-26.667ZM0 213.277l45.333-26.667 45.334 26.667-45.334 26.667L0 213.277ZM136 239.944l-45.333-26.667v53.333L136 239.944Z"
-						fill="#FF5D01"
-					></path>
-					<path
-						d="m136 53.333 45.333-26.666v120L226.667 120V80L272 53.333V160l-90.667 53.333v240L136 480V306.667L45.334 360V240l45.333-26.667v53.334L136 240V53.333Z"
-						fill="#53C68C"
-					></path>
-					<path d="M45.334 240 0 213.334v120L45.334 360V240Z" fill="#B04304"></path>
-				</svg>
-				<p tw="ml-3 font-semibold">${siteConfig.title}</p>
+		<div class="flex w-full items-center justify-between border-t border-[#2bbc89] p-10 text-xl">
+			<div class="flex items-center">
+				<p class="ml-3 font-semibold">${siteConfig.title} Note</p>
 			</div>
 			<p>by ${siteConfig.author}</p>
 		</div>
@@ -61,11 +43,8 @@ type Props = InferGetStaticPropsType<typeof getStaticPaths>;
 
 export async function GET(context: APIContext) {
 	const { title, pubDate } = context.props as Props;
-
-	const postDate = getFormattedDate(pubDate, {
-		weekday: "long",
-		month: "long",
-	});
+	console.log(title, pubDate);
+	const postDate = format(pubDate, "MMM D, YYYY");
 	const svg = await satori(markup(title, postDate), ogOptions);
 	const png = new Resvg(svg).render().asPng();
 	return new Response(png, {
